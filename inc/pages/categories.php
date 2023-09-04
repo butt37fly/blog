6576f4cc9e3a250ebf9fae6ff0fff0e9b0865ecc
butt37fly <adrosscosmic@gmail.com>
2023-09-04 15:40:12 -0500

    Formulario para crear categorías creado

---
+++ inc/pages/categories.php
@@ -0,0 +1,16 @@
+<form class="Form" method="POST" action="<?php echo SITE_URI ."category/" ?>">
+  <div class="Form__wrapper">
+    <h3 class="Form__title Text">Crear categoría</h3>
+    <section class="Form__section">
+      <label class="Form__label Label" for="CategoryName">Nombre</label>
+      <input class="Form__input Input" type="text" name="CategoryName" id="CategoryName" placeholder="Mi categoría" required>
+    </section>
+    <section class="Form__section">
+      <label class="Form__label Label" for="CategorySlug">Slug (opcional)</label>
+      <input class="Form__input Input" type="text" name="CategorySlug" id="CategorySlug" placeholder="mi-categoria">
+    </section>
+    <section class="Form__section">
+      <input class="Form__submit Button" type="submit" name="CreateCategory" value="Crear categoría">
+    </section>
+  </div>
+</form>
