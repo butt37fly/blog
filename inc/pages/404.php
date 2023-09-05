289c5a5531fbf306acf9d1b1910ef86ee43e2485
butt37fly <adrosscosmic@gmail.com>
2023-09-05 15:44:17 -0500

    Plantilla para página 404

---
+++ inc/pages/404.php
@@ -0,0 +1,5 @@
+<section class="Not-Found">
+  <h1 class="Not-Found__title Title">
+    Oops!, parece que este sitio no existe :(
+  </h1>
+</section>
--- inc/templates/content.php
+++ inc/templates/content.php
@@ -2,19 +2,32 @@
 
   <?php
 
-  if ( ! isset( $_GET['page'] )) redirect_to();
+  $pages = ["home", "categories"];
+
+  if (!isset($_GET['page']))
+    redirect_to();
+
+  if (!in_array($_GET['page'], $pages)) {
+
+    include 'inc/pages/404.php';
+
+  }
 
   if ($_GET['page'] === "home") {
 
     include 'inc/pages/home.php';
-    
+
   }
-  
+
   if ($_GET['page'] === "categories") {
-    
+
     if (is_user_logged()) {
-      
+
       include 'inc/pages/categories.php';
+
+    } else {
+
+      redirect_to();
 
     }
   }
