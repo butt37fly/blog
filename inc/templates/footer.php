e3842f97d887b1dd646d06ec0f7dc6b812a6e8f8
butt37fly <adrosscosmic@gmail.com>
2023-09-11 11:31:19 -0500

    Enlaces sociales

--- inc/templates/footer.php
+++ inc/templates/footer.php
@@ -6,18 +6,15 @@
       </section>
       <section class="Footer__wrapper Footer__wrapper--bottom">
         <div class="Footer__info">
-          <h3 class="Footer__title Text">Diseñado y desarrollado por <a class="Footer__link Link" href="#">butt37fly</a>
+          <h3 class="Footer__title Text">Diseñado y desarrollado por <a class="Footer__link Link" href="https://butt37fly.github.io/Portfolio/" target="_blank">butt37fly</a>
           </h3>
           <nav class="Nav">
             <ul class="Nav__wrapper Nav__wrapper--center">
               <li class="Nav__item">
-                <a href="#" class="Link">Github</a>
+                <a href="https://github.com/butt37fly/blog" target="_blank" class="Link">Github</a>
               </li>
               <li class="Nav__item">
-                <a href="#" class="Link">LinkedIn</a>
-              </li>
-              <li class="Nav__item">
-                <a href="#" class="Link">Behance</a>
+                <a href="https://www.linkedin.com/in/butt37fly/" target="_blank" class="Link">LinkedIn</a>
               </li>
             </ul>
           </nav>
